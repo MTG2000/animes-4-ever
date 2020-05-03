91694d5ca0e05e352564ae69cdcf8813a4597916
=Mtg_Dev <=mtg0987654321@gmail.com>
2020-05-03 21:53:23 +0200

    add build folder

---
+++ client/build/precache-manifest.26e66901ea16adef871d8d8f2a636fe6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c276ddf088c58a474789d730e8f61cfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60fc58d088eb32c8a358",
+    "url": "/static/css/main.f3c611bb.chunk.css"
+  },
+  {
+    "revision": "228748d345462115ff58",
+    "url": "/static/js/2.9539866e.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.9539866e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a823ebd72bc172dd4a8",
+    "url": "/static/js/3.c995ac06.chunk.js"
+  },
+  {
+    "revision": "60fc58d088eb32c8a358",
+    "url": "/static/js/main.3308d679.chunk.js"
+  },
+  {
+    "revision": "241b4b63ae4d26b2bad8",
+    "url": "/static/js/runtime-main.74b3062c.js"
+  }
+]);
